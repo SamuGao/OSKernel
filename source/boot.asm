91801fe63e4fb37e56b4b3f3ee0610a683d854f2
SamuGao <gaoxuanhui@foxmail.com>
2017-03-05 00:31:58 +0800

    添加实模式跳转代码

--- source/boot.asm
+++ source/boot.asm
@@ -1,7 +1,6 @@
-  BOOTSEG = 07C00H
-  SYSSEG  = 01000H
-  SYSLEN  = 17
-  org BOOTSEG
+include 'Structures.inc'
+include 'Constant.inc'
+org BOOTSEG
   use16
   mov ax, BOOTSEG
   mov ds, ax
@@ -19,7 +18,7 @@ LoadSystem:
 
   jc ErrInfo
 SysStart:
-  jmp 01000h:0000h
+  jmp SYSSEG:0000h
 ErrInfo:
   mov ax, cs
   mov ds, ax
@@ -41,18 +40,109 @@ times 510-($-$$) db 0
 dw 0xaa55
 
 org 0h
-  mov ax, cs
-  mov ds, ax
-  mov es, ax
-  call ShowHello
-  jmp $
-ShowHello:
-  mov ax, HelloMessage
-  mov bp, ax
+	jmp	LABEL_BEGIN ;跳转到开始处执行
+
+; 全局描述符定义
+LABEL_GDT   	   Descriptor 0, 0, 0, 0, 0  ; 空描述符
+LABEL_DESC_CODE32  Descriptor 0, 0, 0, 0, 0  ; 非一致代码段
+LABEL_DESC_VIDEO   Descriptor 0, 0, 0, 0, 0  ; 显存首地址
+
+GdtLen		equ	$ - LABEL_GDT	; 全局描述符长度
+GdtPtr		dw	GdtLen - 1	; 全局描述符界限
+		      dd	0		; GDT基地址
+
+; 描述符选择子
+SelectorCode32		equ	LABEL_DESC_CODE32	- LABEL_GDT
+SelectorVideo		equ	LABEL_DESC_VIDEO	- LABEL_GDT
+
+LABEL_BEGIN:
+  use16
+	mov	ax, cs
+	mov	ds, ax
+	mov	es, ax
+	mov	ss, ax
+	mov	sp, 0100h
+
+	; 初始化 32 位代码段描述符
+	mov word [LABEL_DESC_CODE32], SegCode32Len - 1 ; 段界限0~15
+	xor	eax, eax
+	mov	ax, cs
+	shl	eax, 4
+	add	eax, LABEL_SEG_CODE32
+	mov	word [LABEL_DESC_CODE32 + 2], ax ;段基址0~15
+	shr	eax, 16
+	mov	byte [LABEL_DESC_CODE32 + 4], al ;段基址16~23
+	mov	byte [LABEL_DESC_CODE32 + 7], ah ;段基址24~31
+	mov eax, SegCode32Len - 1
+	shr eax, 8
+	and ax, 0F00h
+	mov bx, DA_C + DA_32
+	or ax, bx
+	mov word [LABEL_DESC_CODE32 + 5], ax
+
+	; 初始化视频数据段段描述符
+	mov word [LABEL_DESC_VIDEO], 0ffffh ; 段界限0~15
+	xor	eax, eax
+	mov	eax, 0B8000h
+	mov	word [LABEL_DESC_VIDEO + 2], ax ;段基址0~15
+	shr	eax, 16
+	mov	byte [LABEL_DESC_VIDEO + 4], al ;段基址16~23
+	mov	byte [LABEL_DESC_VIDEO + 7], ah ;段基址24~31
+	mov eax, 0ffffh
+	shr eax, 8
+	and ax, 0F00h
+	mov bx, DA_DRW
+	or ax, bx
+	mov word [LABEL_DESC_VIDEO + 5], ax
+
+	; 为加载 GDTR 作准备
+	xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, LABEL_GDT		; eax <- gdt 基地址
+	mov	dword [GdtPtr + 2], eax	; [GdtPtr + 2] <- gdt 基地址
+
+	; 加载 GDTR
+	lgdt fword [GdtPtr]
+
+	; 关中断
+	cli
+
+	; 打开地址线A20
+	in	al, 92h
+	or	al, 00000010b
+	out	92h, al
+
+	; 准备切换到保护模式
+	mov	eax, cr0
+	or	eax, 1
+	mov	cr0, eax
+
+	; 真正进入保护模式
+	jmp	dword SelectorCode32:0	; 执行这一句会把 SelectorCode32 装入 cs,
+					; 并跳转到 Code32Selector:0  处
+; END of [SECTION .s16]
+
+
+use32
+
+LABEL_SEG_CODE32:
+	mov	ax, SelectorVideo
+	mov	gs, ax			; 视频段选择子(目的)
+
+  cld
+  mov si, WelcomeMessage
+  mov ebx, (80 * 0 + 0) * 2 ;屏幕第 0 行, 第 0 列
   mov cx, 18
-  mov ax, 01301h    
-  mov bx, 000ch
-  mov dx, 0000h
-  int 10h
-  ret
-  HelloMessage db "Welcome to SemiOS!"
+SHOW_WELCOME:
+	mov	edi, ebx
+	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
+	lodsb
+	mov	[gs:edi], ax
+  add ebx, 2
+  loop SHOW_WELCOME
+
+	; 到此停止
+	jmp	$
+WelcomeMessage db 'Welcome to SemiOS!'
+SegCode32Len	=	$ - LABEL_SEG_CODE32
